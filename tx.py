8dcf42fb7f43468349742c006fb9389a94cc479c
Jimmy Song <jaejoon@gmail.com>
2017-10-02 23:11:15 -0700

    session 4 complete

--- tx.py
+++ tx.py
@@ -42,23 +42,57 @@ class Tx:
     def serialize(self):
         '''Returns the byte serialization of the transaction'''
         # version
+        result = int_to_little_endian(self.version, 4)
         # inputs
+        result += bytes([len(self.tx_ins)])
+        for tx_in in self.tx_ins:
+            result += tx_in.serialize()
         # outputs
+        result += bytes([len(self.tx_outs)])
+        for tx_out in self.tx_outs:
+            result += tx_out.serialize()
         # locktime
-        raise NotImplementedError
+        result += int_to_little_endian(self.locktime, 4)
+        return result
 
     def fee(self):
         '''Returns the fee of this transaction in satoshi'''
-        raise NotImplementedError
+        input_sum = 0
+        for tx_in in self.tx_ins:
+            input_sum += tx_in.value(self.testnet)
+        output_sum = 0
+        for tx_out in self.tx_outs:
+            output_sum += tx_out.amount
+        return input_sum - output_sum
 
     def hash_to_sign(self, input_index, sighash):
         '''Returns the integer representation of the hash that needs to get
         signed for index input_index'''
         # create a transaction serialization where
         # all the input script_sigs are blanked out
+        alt_tx_ins = []
+        for tx_in in self.tx_ins:
+            alt_tx_ins.append(TxIn(
+                prev_tx=tx_in.prev_tx,
+                prev_index=tx_in.prev_index,
+                script_sig=b'',
+                sequence=tx_in.sequence,
+            ))
         # replace the input's scriptSig with the scriptPubKey
+        signing_input = alt_tx_ins[input_index]
+        signing_input.script_sig = Script.parse(
+            signing_input.script_pubkey(self.testnet))
+        alt_tx = self.__class__(
+            version=self.version,
+            tx_ins=alt_tx_ins,
+            tx_outs=self.tx_outs,
+            locktime=self.locktime)
         # add the sighash
-        raise NotImplementedError
+        result = alt_tx.serialize() + int_to_little_endian(sighash, 4)
+        return int.from_bytes(double_sha256(result), 'big')
+
+
+CACHE = {'75d7454b7010fa28b00f16cccb640b1756fd6e357c03a3b81b9d119505f47b56:0': {'script': '76a914cd0b3a22cd16e182291aa2708c41cb38de5a330788ac', 'addresses': ['1KhAyQ3kaRQptGwAZghHBjNg65dgGdDXak'], 'value': 1043341, 'script_type': 'pay-to-pubkey-hash', 'spent_by': 'ee51510d7bbabe28052038d1deb10c03ec74f06a79e21913c6fcf48d56217c87'}, 'd1c789a9c60383bf715f3f6ad9d14b91fe55f3deb369fe5d9280cb1a01793f81:0': {'script': '76a914a802fc56c704ce87c42d7c92eb75e7896bdc41ae88ac', 'addresses': ['1GKN6gJBgvet8S92qiQjVxEaVJ5eoJE9s2'], 'value': 42505594, 'script_type': 'pay-to-pubkey-hash', 'spent_by': '452c629d67e41baec3ac6f04fe744b4b9617f8f859c63b3002f8684e7a4fee03'}, 'd37f9e7282f81b7fd3af0fde8b462a1c28024f1d83cf13637ec18d03f4518fe:0': {'script': '76a914af24b3f3e987c23528b366122a7ed2af199b36bc88ac', 'addresses': ['1Gy5Djegn51WxHQN4X19FBsUy8RQ74hvYo'], 'value': 29960102, 'script_type': 'pay-to-pubkey-hash', 'spent_by': 'ee51510d7bbabe28052038d1deb10c03ec74f06a79e21913c6fcf48d56217c87'}, '45f3f79066d251addc04fd889f776c73afab1cb22559376ff820e6166c5e3ad6:1': {'script': '76a914311b232c3400080eb2636edb8548b47f6835be7688ac', 'addresses': ['15UecwTDg57tnfSM6Cra8cmZVYavxtTZp2'], 'value': 9337330, 'script_type': 'pay-to-pubkey-hash', 'spent_by': 'ee51510d7bbabe28052038d1deb10c03ec74f06a79e21913c6fcf48d56217c87'}, '9e067aedc661fca148e13953df75f8ca6eada9ce3b3d8d68631769ac60999156:1': {'script': '76a914677345c7376dfda2c52ad9b6a153b643b6409a3788ac', 'addresses': ['1ARzh3A5fgGzbaXkg3novtH8AopzojY79D'], 'value': 800000, 'script_type': 'pay-to-pubkey-hash', 'spent_by': 'ee51510d7bbabe28052038d1deb10c03ec74f06a79e21913c6fcf48d56217c87'}, '0025bc3c0fa8b7eb55b9437fdbd016870d18e0df0ace7bc9864efc38414147c8:0': {'script': '76a914d52ad7ca9b3d096a38e752c2018e6fbc40cdf26f88ac', 'value': 110000000, 'script_type': 'pay-to-pubkey-hash', 'addresses': ['mzx5YhAH9kNHtcN481u6WkjeHjYtVeKVh2']}}
 
 
 class TxIn:
@@ -84,10 +118,32 @@ class TxIn:
 
     def serialize(self):
         '''Returns the byte serialization of the transaction input'''
-        # tx and index, prev_tx is little-endian!
-        # += script_sig.serialize()
+        # tx, prev_tx is little-endian!
+        result = self.prev_tx[::-1] + int_to_little_endian(self.prev_index, 4)
+        # script_sig
+        serialized_script_sig = self.script_sig.serialize()
+        result += bytes([len(serialized_script_sig)]) + serialized_script_sig
         # sequence
-        raise NotImplementedError
+        result += int_to_little_endian(self.sequence, 4)
+        return result
+
+    def outpoint(self, testnet=False):
+        cache_key = '{}:{}'.format(
+            hexlify(self.prev_tx).decode('ascii'), self.prev_index)
+        cache = CACHE.get(cache_key)
+        if cache:
+            return cache
+        if testnet:
+            net = 'test3'
+        else:
+            net = 'main'
+        url = 'https://api.blockcypher.com/v1/btc/{}/txs/{}?token=41298c19cc85400da2f1aa620578b096'.format(
+            net, hexlify(self.prev_tx).decode('ascii'))
+        tx_json = requests.get(url).json()
+        if 'outputs' not in tx_json:
+            raise RuntimeError('received {}'.format(tx_json))
+        CACHE[cache_key] = tx_json['outputs'][self.prev_index]
+        return CACHE[cache_key]
 
     def value(self, testnet=False):
         '''tx_hash is a hex version of tx, index is an integer
@@ -96,7 +152,8 @@ class TxIn:
         '''
         # might be useful
         # requests.get(url).json()
-        raise NotImplementedError
+        outpoint = self.outpoint(testnet=testnet)
+        return outpoint['value']
 
     def script_pubkey(self, testnet=False):
         '''tx_hash is a hex version of tx, index is an integer
@@ -105,7 +162,8 @@ class TxIn:
         '''
         # might be useful
         # requests.get(url).json()
-        raise NotImplementedError
+        outpoint = self.outpoint(testnet=testnet)
+        return unhexlify(outpoint['script'])
 
     def der_signature(self, index=0):
         '''returns a DER format signature and sighash if the script_sig
@@ -138,8 +196,11 @@ class TxOut:
     def serialize(self):
         '''Returns the byte serialization of the transaction output'''
         # amount
-        # script_pubkey.serialize()
-        raise NotImplementedError
+        result = int_to_little_endian(self.amount, 8)
+        # pubkey
+        serialized_script_pubkey = self.script_pubkey.serialize()
+        result += bytes([len(serialized_script_pubkey)]) + serialized_script_pubkey
+        return result
 
 
 class TxTest(TestCase):
@@ -198,14 +259,12 @@ class TxTest(TestCase):
         want = b'0349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278a'
         self.assertEqual(hexlify(tx.tx_ins[0].sec_pubkey()), want)
 
-    @skip('unimplemented')
     def test_serialize(self):
         raw_tx = unhexlify('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
         stream = BytesIO(raw_tx)
         tx = Tx.parse(stream)
         self.assertEqual(tx.serialize(), raw_tx)
 
-    @skip('unimplemented')
     def test_input_value(self):
         tx_hash = 'd1c789a9c60383bf715f3f6ad9d14b91fe55f3deb369fe5d9280cb1a01793f81'
         index = 0
@@ -218,7 +277,6 @@ class TxTest(TestCase):
         )
         self.assertEqual(tx_in.value(), want)
 
-    @skip('unimplemented')
     def test_input_pubkey(self):
         tx_hash = 'd1c789a9c60383bf715f3f6ad9d14b91fe55f3deb369fe5d9280cb1a01793f81'
         index = 0
@@ -231,7 +289,6 @@ class TxTest(TestCase):
         want = unhexlify('76a914a802fc56c704ce87c42d7c92eb75e7896bdc41ae88ac')
         self.assertEqual(tx_in.script_pubkey(), want)
 
-    @skip('unimplemented')
     def test_fee(self):
         raw_tx = unhexlify('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
         stream = BytesIO(raw_tx)
@@ -242,7 +299,6 @@ class TxTest(TestCase):
         tx = Tx.parse(stream)
         self.assertEqual(tx.fee(), 140500)
 
-    @skip('unimplemented')
     def test_hash_to_sign(self):
         raw_tx = unhexlify('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
         stream = BytesIO(raw_tx)
