9ac066aa533f603638e1652cea058daa6c47015e
Jimmy Song <jaejoon@gmail.com>
2017-10-02 22:15:45 -0700

    session 1 complete

--- ecc.py
+++ ecc.py
@@ -25,27 +25,29 @@ class FieldElement:
         return 'FieldElement_{}({})'.format(self.prime, self.num)
 
     def __add__(self, other):
-        raise NotImplementedError
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num=num, prime=self.prime)
 
     def __sub__(self, other):
-        raise NotImplementedError
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num=num, prime=self.prime)
 
     def __mul__(self, other):
-        raise NotImplementedError
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num=num, prime=self.prime)
 
     def __pow__(self, n):
-        # remember fermat's little theorem self.num**(p-1) is equivalent to 1
-        # you might want to use % operator
-        raise NotImplementedError
+        n = n % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num=num, prime=self.prime)
 
     def __truediv__(self, other):
-        raise NotImplementedError
-
+        other_inv = pow(other.num, self.prime - 2, self.prime)
+        return self*self.__class__(num=other_inv, prime=self.prime)
 
 
 class FieldElementTest(TestCase):
 
-    @skip('unimplemented')
     def test_add(self):
         a = FieldElement(2, 31)
         b = FieldElement(15, 31)
@@ -54,7 +56,6 @@ class FieldElementTest(TestCase):
         b = FieldElement(21, 31)
         self.assertEqual(a+b, FieldElement(7, 31))
 
-    @skip('unimplemented')
     def test_sub(self):
         a = FieldElement(29, 31)
         b = FieldElement(4, 31)
@@ -63,13 +64,11 @@ class FieldElementTest(TestCase):
         b = FieldElement(30, 31)
         self.assertEqual(a-b, FieldElement(16, 31))
 
-    @skip('unimplemented')
     def test_mul(self):
         a = FieldElement(24, 31)
         b = FieldElement(19, 31)
         self.assertEqual(a*b, FieldElement(22, 31))
-        
-    @skip('unimplemented')
+
     def test_pow(self):
         a = FieldElement(17, 31)
         self.assertEqual(a**3, FieldElement(15, 31))
@@ -77,7 +76,6 @@ class FieldElementTest(TestCase):
         b = FieldElement(18, 31)
         self.assertEqual(a**5 * b, FieldElement(16, 31))
 
-    @skip('unimplemented')
     def test_div(self):
         a = FieldElement(3, 31)
         b = FieldElement(24, 31)
@@ -89,14 +87,20 @@ class FieldElementTest(TestCase):
         self.assertEqual(a**-4*b, FieldElement(13, 31))
 
 
-
 class Point:
 
     def __init__(self, x, y, a, b):
-        self.x = x
-        self.y = y
         self.a = a
         self.b = b
+        if x is None and y is None:
+            # point at infinity
+            self.x = None
+            self.y = None
+            return
+        if y**2 != x**3 + self.a * x + self.b:
+            raise RuntimeError('Not a point on the curve')
+        self.x = x
+        self.y = y
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y \
@@ -113,12 +117,29 @@ class Point:
             return 'Point({},{})'.format(self.x, self.y)
 
     def __add__(self, other):
-        raise NotImplementedError
+        # identity
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        if self.x == other.x:
+            if self.y != other.y:
+                # point at infinity
+                return self.__class__(x=None, y=None, a=self.a, b=self.b)
+            # we're adding a point to itself
+            s = (3* self.x**2 + self.a) / (2* self.y)
+            x = s**2 - 2*self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, a=self.a, b=self.b)
+        else:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x=x, y=y, a=self.a, b=self.b)
 
 
 class PointTest(TestCase):
 
-    @skip('unimplemented')
     def test_on_curve(self):
         with self.assertRaises(RuntimeError):
             Point(x=-2, y=4, a=5, b=7)
@@ -127,7 +148,6 @@ class PointTest(TestCase):
         Point(x=18, y=77, a=5, b=7)
         Point(x=None, y=None, a=5, b=7)
 
-    @skip('unimplemented')
     def test_add0(self):
         a = Point(x=None, y=None, a=5, b=7)
         b = Point(x=2, y=5, a=5, b=7)
@@ -135,14 +155,12 @@ class PointTest(TestCase):
         self.assertEqual(a+b, b)
         self.assertEqual(b+a, b)
         self.assertEqual(b+c, a)
-    
-    @skip('unimplemented')
+
     def test_add1(self):
         a = Point(x=3, y=7, a=5, b=7)
         b = Point(x=-1, y=-1, a=5, b=7)
         self.assertEqual(a+b, Point(x=2, y=-5, a=5, b=7))
 
-    @skip('unimplemented')
     def test_add2(self):
         a = Point(x=-1, y=1, a=5, b=7)
         self.assertEqual(a+a, Point(x=18, y=-77, a=5, b=7))
